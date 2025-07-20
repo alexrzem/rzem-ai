fbf7ea11bb8d45e9a3a270f3abadd43635716cd1
Alex <alexrzem@users.noreply.github.com>
2025-07-21 00:24:48 +1000

    updates

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -2,24 +2,38 @@ import { NavbarOptions } from 'vuepress-theme-hope/client';
 
 export const navbar: NavbarOptions = [
     {
-        text: 'Home',
+        text: 'home',
         link: '/',
     },
     {
-        text: 'Blueprints',
+        text: 'online tools',
         children: [
-			{ text: 'AI Risk Architect', link: '/blueprints/ai_risk_architect/', icon: 'regular:file-image' },
-			{ text: 'AI Regulation', link: '/blueprints/ai_regulation/', icon: 'regular:file-image' }
-		],
+            { text: 'Overview', link: '/online/', icon: 'regular:file-image' },
+			{ text: 'Midjourney', link: '/online/midjourney/', icon: 'regular:file-image' },
+        ],
     },
-
     {
-        text: 'Txt2Img',
+        text: 'desktop tools',
         children: [
-            { text: 'Image Tools', link: '/txt2img/txt2img.md', icon: 'regular:file-image' },
-            { text: 'Flux1', link: '/txt2img/flux1/', icon: 'regular:file-image' },
-            { text: 'Midjourney', link: '/txt2img/midjourney/', icon: 'regular:file-image' },
-            { text: 'Stable Diffusion', link: '/txt2img/sd/', icon: 'regular:file-image' },
+			{ text: 'Overview', link: '/desktop/', icon: 'regular:file-image' },
+            { text: 'Flux1', link: '/desktop/flux1/', icon: 'regular:file-image' },
+            { text: 'Stable Diffusion', link: '/desktop/sd/', icon: 'regular:file-image' },
+        ],
+    },
+    {
+        text: 'image-to',
+        children: [
+			{ text: 'Overview', link: '/image-to/', icon: 'regular:file-image' },
+            { text: 'Link 1', link: '/image-to/page1.md', icon: 'regular:file-image' },
+            { text: 'Link 2', link: '/image-to/page2.md', icon: 'regular:file-image' },
+        ],
+    },
+    {
+        text: 'text-to',
+        children: [
+			{ text: 'Overview', link: '/text-to/', icon: 'regular:file-image' },
+            { text: 'Link 1', link: '/text-to/page1.md', icon: 'regular:file-image' },
+            { text: 'Link 2', link: '/text-to/page2.md', icon: 'regular:file-image' },
         ],
     },
 ] as NavbarOptions;
--- docs/blueprints/readme.md
+++
@@ -1,4 +0,0 @@
----
-title: Blueprints
-icon: regular:file
----
