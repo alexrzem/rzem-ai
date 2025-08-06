eacea3c1c10607abbc637befc4ed163bad2f9b3b
Alex <alex@rzem.com>
2025-08-06 17:06:36 +1000

    updates

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
             <div>
                 <MegaMenu :model="items">
                     <template #itemicon="{ item }">
-                        <fa v-if="item.icon" :icon="item.icon" class="text-base" />
+                        <fa v-if="item.icon" :icon="item.icon" class="text-2xl text-blue-500 hover:text-blue-300" />
                     </template>
                 </MegaMenu>
             </div>
@@ -31,42 +31,104 @@ const styles = ref([
 
 const items = ref([
     {
-        label: '',
-        icon: ['fa-light','fa-home'],
+        label: 'Home',
+        icon: ['fa-duotone', 'fa-passport'],
     },
     {
         label: 'Online',
-        icon: ['fa-light','fa-cloud'],
+        icon: ['fa-duotone', 'fa-cloud'],
         items: [
+            // {
+            //     text: 'ChatGPT',
+            //     icon: 'fluent-color:link-24',
+            //     link: 'https://sora.chatgpt.com/',
+            // },
+            // {
+            //     text: 'Comfy.icu',
+            //     icon: 'fluent-color:link-24',
+            //     link: 'https://comfy.icu/',
+            // },
+            // {
+            //     text: 'Dreamina',
+            //     icon: 'fluent-color:link-24',
+            //     link: 'https://dreamina.capcut.com/ai-tool/image/generate',
+            // },
+
+            // {
+            //     text: 'fal.ai',
+            //     icon: 'fluent-color:link-24',
+            //     link: 'https://fal.ai/models',
+            // },
+            // {
+            //     text: 'Image-Fx',
+            //     icon: 'fluent-color:link-24',
+            //     link: 'https://labs.google/fx/tools/image-fx',
+            // },
+            // {
+            //     text: 'Midjourney',
+            //     icon: 'fluent-color:link-24',
+            //     link: 'https://www.midjourney.com/explore',
+            // },
+            // {
+            //     text: 'OpenArt.ai',
+            //     icon: 'fluent-color:link-24',
+            //     link: 'https://openart.ai/create',
+            // },
+            // {
+            //     text: 'Runwayml',
+            //     icon: 'fluent-color:link-24',
+            //     link: 'https://runwayml.com',
+            // },
+
             [
                 {
-                    label: 'Computer',
-                    items: [{ label: 'Monitor' }, { label: 'Mouse' }, { label: 'Notebook' }, { label: 'Keyboard' }, { label: 'Printer' }, { label: 'Storage' }],
+                    label: 'Large',
+                    items: [
+                        { label: 'ChatGPT', icon: ['fa-light', 'fa-square-dollar'] },
+                        { label: 'Dreamina', icon: ['fa-light', 'fa-square-dollar'] },
+                        { label: 'Image-Fx', icon: ['fa-light', 'fa-square-dollar'] },
+                        { label: 'Runwayml', icon: ['fa-light', 'fa-square-dollar'] },
+                    ],
                 },
             ],
             [
                 {
-                    label: 'Home Theater',
-                    items: [{ label: 'Projector' }, { label: 'Speakers' }, { label: 'TVs' }],
+                    label: 'Midjourney',
+                    items: [
+                        { label: 'Projector', icon: ['fa-light', 'fa-square'] },
+                        { label: 'Speakers', icon: ['fa-light', 'fa-square'] },
+                        { label: 'TVs', icon: ['fa-light', 'fa-square'] },
+                    ],
                 },
             ],
             [
                 {
-                    label: 'Gaming',
-                    items: [{ label: 'Accessories' }, { label: 'Console' }, { label: 'PC' }, { label: 'Video Games' }],
+                    label: 'Platforms',
+                    items: [
+                        { label: 'fal.ai', icon: ['fa-light', 'fa-cubes'] },
+                        { label: 'Comfy.icu', icon: ['fa-light', 'fa-cubes'] },
+                        { label: 'Huggingface', icon: ['fa-light', 'fa-cubes'] },
+                        { label: 'Replicate', icon: ['fa-light', 'fa-cubes'] },
+                    ],
                 },
             ],
+
             [
                 {
-                    label: 'Appliances',
-                    items: [{ label: 'Coffee Machine' }, { label: 'Fridge' }, { label: 'Oven' }, { label: 'Vaccum Cleaner' }, { label: 'Washing Machine' }],
+                    label: 'Communities',
+                    items: [
+                        { label: 'OpenArt.ai', icon: ['fa-light', 'fa-users-line'] },
+                        { label: 'Comfy.icu', icon: ['fa-light', 'fa-users-line'] },
+                        { label: 'Huggingface', icon: ['fa-light', 'fa-users-line'] },
+                        { label: 'tensor.art', icon: ['fa-light', 'fa-users-line'] },
+                    ],
                 },
             ],
         ],
     },
     {
         label: 'Desktop',
-        icon: ['fa-light','fa-computer-classic'],
+        icon: ['fa-duotone', 'fa-computer-classic'],
         items: [
             [
                 {
@@ -96,29 +158,38 @@ const items = ref([
     },
     {
         label: 'Technology',
-        icon: ['fa-light','fa-flux-capacitor'],
+        icon: ['fa-duotone', 'fa-flux-capacitor'],
         items: [
+            // { text: 'FLUX', link: '/tech/flux/', icon: 'fluent-color:code-block-16' },
+            //     { text: 'Stable Diffusion', link: '/tech/sd/', icon: 'fluent-color:code-block-16' },
+            //     { text: 'Text-To-Image', link: '/tech/text-to/text-to-image.md', icon: 'fluent-color:text-bullet-list-square-16' },
+            //     { text: 'Text-To-Music', link: '/tech/text-to/text-to-music.md', icon: 'fluent-color:text-bullet-list-square-16' },
+            //     { text: 'Text-To-Video', link: '/tech/text-to/text-to-video.md', icon: 'fluent-color:text-bullet-list-square-16' },
+            //     { text: 'Image-To-Image', link: '/tech/image-to/image-to-image.md', icon: 'fluent-color:image-16' },
+            //     { text: 'Image-To-Text', link: '/tech/image-to/image-to-text.md', icon: 'fluent-color:image-16' },
+            //     { text: 'Image-To-Video', link: '/tech/image-to/image-to-video.md', icon: 'fluent-color:image-16' },
+
             [
                 {
-                    label: 'Football',
+                    label: 'FLUX',
                     items: [{ label: 'Kits' }, { label: 'Shoes' }, { label: 'Shorts' }, { label: 'Training' }],
                 },
             ],
             [
                 {
-                    label: 'Running',
+                    label: 'Stable Diffusion',
                     items: [{ label: 'Accessories' }, { label: 'Shoes' }, { label: 'T-Shirts' }, { label: 'Shorts' }],
                 },
             ],
             [
                 {
-                    label: 'Swimming',
+                    label: 'Text-To',
                     items: [{ label: 'Kickboard' }, { label: 'Nose Clip' }, { label: 'Swimsuits' }, { label: 'Paddles' }],
                 },
             ],
             [
                 {
-                    label: 'Tennis',
+                    label: 'Image-To',
                     items: [{ label: 'Balls' }, { label: 'Rackets' }, { label: 'Shoes' }, { label: 'Training' }],
                 },
             ],
@@ -126,7 +197,7 @@ const items = ref([
     },
     {
         label: 'Portfolio',
-        icon: ['fa-duotone','fa-solid','fa-image'],
+        icon: ['fa-duotone', 'fa-image'],
     },
 ]);
 </script>
--- src/style.ts
+++ src/style.ts
@@ -4,44 +4,27 @@ import './assets/styles/main.css';
 
 /* import the fontawesome core */
 /* import icons and add them to the Library */
-import {} from '@awesome.me/kit-a6cc1df5fb/icons/whiteboard/semibold';
-
+import * as Icons from '@awesome.me/kit-a6cc1df5fb/icons';
 import { library } from '@fortawesome/fontawesome-svg-core';
-// import { fal } from '@fortawesome/pro-light-svg-icons';
-// import { far } from '@fortawesome/pro-regular-svg-icons';
-// import { fass } from '@fortawesome/sharp-solid-svg-icons';
-// import { fad } from '@fortawesome/pro-duotone-svg-icons';
-// import { fadt } from '@fortawesome/duotone-thin-svg-icons';
 
-
-//library.add(fal, far, fass, fad, fadt);
-
-/* add icons to the library */
-// library.add(fab);
-// library.add(fad);
-// library.add(fal);
-// library.add(far);
-// library.add(fas);
-//library.add(fonts);
-
-console.log('FontAwesome icons loaded:', fonts);
+library.add(Icons.fab, Icons.facr, Icons.fad, Icons.fadl, Icons.fadr, Icons.fadt, Icons.faes, Icons.fal, Icons.far, Icons.fas, Icons.fasl, Icons.fat, Icons.fawsb);
 
 import type { Preset } from '@primeuix/themes/types';
 
 const preset: Preset = {
     semantic: {
         primary: {
-            50: '{indigo.50}',
-            100: '{indigo.100}',
-            200: '{indigo.200}',
-            300: '{indigo.300}',
-            400: '{indigo.400}',
-            500: '{indigo.500}',
-            600: '{indigo.600}',
-            700: '{indigo.700}',
-            800: '{indigo.800}',
-            900: '{indigo.900}',
-            950: '{indigo.950}',
+            50: '{blue.50}',
+            100: '{blue.100}',
+            200: '{blue.200}',
+            300: '{blue.300}',
+            400: '{blue.400}',
+            500: '{blue.500}',
+            600: '{blue.600}',
+            700: '{blue.700}',
+            800: '{blue.800}',
+            900: '{blue.900}',
+            950: '{blue.950}',
         },
         colorScheme: {
             light: {
@@ -63,8 +46,23 @@ const preset: Preset = {
             root: {
                 borderRadius: '1.0rem',
                 background: '{surface.900}',
+                horizontalOrientation: {
+                    padding: '0.3rem'
+                }
+            },
+            baseItem: {
+                padding: '0.6rem',
+                borderRadius: '1.0rem',
+
+            },
+            item: {
+                color: '{zinc.200}',
+                focusBackground: '{primary.950}',
+                focusColor: '{zinc.50}',
+                gap: '0.1rem',
+                padding: '0.1rem',
             }
-        }
+        },
     },
 };
 
