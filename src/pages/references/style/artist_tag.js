58828c23a616c5fda255df69c71ef278b87c3852
Alex <alexrzem@users.noreply.github.com>
2025-08-22 09:59:22 +1000

    artists

--- src/pages/references/style/artist_tag.js
+++ src/pages/references/style/artist_tag.js
@@ -1,19 +1,80 @@
-import { z } from 'zod';
-import { Chat, LMStudioClient } from '@lmstudio/sdk';
-const client = new LMStudioClient();
+import _ from 'lodash';
+import axios from 'axios';
+import fs from 'fs-extra';
 
-const tagSchema = z.object({
-    artist: z.string(),
-    tags: z.array(z.string()),
-});
+const artists = fs.readJSONSync('src/pages/references/style/styles_artists.json');
 
-const chat = Chat.from([
-    { role: 'system', content: 'You are an art historian. Your results should be a JSON formatted list.' },
-    { role: 'user', content: '3 tags for the art style of Slim Aarons' },
-]);
+const request = {
+    model: 'qwen/qwen3-32b',
+    messages: [
+        {
+            role: 'system',
+            content: 'You are an art historian',
+        },
+        {
+            role: 'user',
+            content: '3 hashtags that describe the art style of Slim Aarons',
+        },
+    ],
+    response_format: {
+        type: 'json_schema',
+        json_schema: {
+            name: 'tag_response',
+            strict: 'true',
+            schema: {
+                type: 'object',
+                properties: {
+                    tags: {
+                        type: 'array',
+                        items: {
+                            type: 'string',
+                        },
+                    },
+                },
+                required: ['tags'],
+            },
+        },
+    },
+    temperature: 0.7,
+    max_tokens: 50,
+    stream: false,
+};
 
-const model = await client.llm.model('openai/gpt-oss-20b');
-const result = await model.respond(chat);
+const process = async function () {
+    const updated_artists = [];
 
-console.log(result.content);
-console.log(result.parsed);
+    for (const artist of artists) {
+        console.log(artist);
+
+        const req = _.cloneDeep(request);
+        _.set(req, 'messages[1].content', `3 hashtags that describe the art style of ${artist.name}. The artists name must not for part of the tag.`);
+
+        const response = await axios.post('http://localhost:7654/v1/chat/completions', req);
+        const tagsResponse = _.get(response, 'data.choices[0].message.content');
+        let tags = _.get(JSON.parse(tagsResponse), 'tags', []);
+        console.log(tags);
+
+        tags = _.map(tags, (item) => {
+            return _.trimStart(item, '#');
+        })
+
+        _.set(artist, 'tags', tags);
+        updated_artists.push(artist)
+        
+    }
+
+    fs.writeFileSync('src/pages/references/style/styles_artists2.json', JSON.stringify(updated_artists, null, '  '));
+};
+
+// axios
+//     .post('http://localhost:7654/v1/chat/completions', request)
+//     .then(function (response) {
+//         const tagsResponse = _.get(response, 'data.choices[0].message.content');
+//         const tags = _.get(JSON.parse(tagsResponse), 'tags', []);
+//         console.log(tags);
+//     })
+//     .catch(function (error) {
+//         console.log(error);
+//     });
+
+process();
